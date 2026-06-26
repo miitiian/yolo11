3ca4627f6443383c77f893c12b09b599b4c5990b
UltralyticsAssistant <web@ultralytics.com>
2026-06-26 10:41:00 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,10 +1,22 @@
-from ultralytics.models import YOLO
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+from ultralytics.models import YOLO
 
-if __name__ == '__main__':
-    model = YOLO(model='ultralytics/cfg/models/12/yolo12.yaml')
-    #model.load('yolo11n.pt')
-    model.train(data='./data.yaml', epochs=200, batch=8, device='0', imgsz=640, workers=2, cache=False,
-                amp=True, mosaic=False, project='runs/train3', name='exp')
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+if __name__ == "__main__":
+    model = YOLO(model="ultralytics/cfg/models/12/yolo12.yaml")
+    # model.load('yolo11n.pt')
+    model.train(
+        data="./data.yaml",
+        epochs=200,
+        batch=8,
+        device="0",
+        imgsz=640,
+        workers=2,
+        cache=False,
+        amp=True,
+        mosaic=False,
+        project="runs/train3",
+        name="exp",
+    )
--- ultralytics/nn/attention/attention2.py
+++ ultralytics/nn/attention/attention2.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
 ######################  CARAFE   ####     start     ###############################
 
 
 class CARAFE(nn.Module):
     # CARAFE: Content-Aware ReAssembly of FEatures       https://arxiv.org/pdf/1905.02188.pdf
     def __init__(self, c1, c2, kernel_size=3, up_factor=2):
-        super(CARAFE, self).__init__()
+        super().__init__()
         self.kernel_size = kernel_size
         self.up_factor = up_factor
         self.down = nn.Conv2d(c1, c1 // 4, 1)
-        self.encoder = nn.Conv2d(c1 // 4, self.up_factor ** 2 * self.kernel_size ** 2,
-                                 self.kernel_size, 1, self.kernel_size // 2)
+        self.encoder = nn.Conv2d(
+            c1 // 4, self.up_factor**2 * self.kernel_size**2, self.kernel_size, 1, self.kernel_size // 2
+        )
         self.out = nn.Conv2d(c1, c2, 1)
 
     def forward(self, x):
@@ -27,15 +27,17 @@ class CARAFE(nn.Module):
         kernel_tensor = F.softmax(kernel_tensor, dim=1)  # (N, Kup^2, S*H, S*W)
         kernel_tensor = kernel_tensor.unfold(2, self.up_factor, step=self.up_factor)  # (N, Kup^2, H, W*S, S)
         kernel_tensor = kernel_tensor.unfold(3, self.up_factor, step=self.up_factor)  # (N, Kup^2, H, W, S, S)
-        kernel_tensor = kernel_tensor.reshape(N, self.kernel_size ** 2, H, W,
-                                              self.up_factor ** 2)  # (N, Kup^2, H, W, S^2)
+        kernel_tensor = kernel_tensor.reshape(N, self.kernel_size**2, H, W, self.up_factor**2)  # (N, Kup^2, H, W, S^2)
         kernel_tensor = kernel_tensor.permute(0, 2, 3, 1, 4)  # (N, H, W, Kup^2, S^2)
 
         # content-aware reassembly module
         # tensor.unfold: dim, size, step
-        x = F.pad(x, pad=(self.kernel_size // 2, self.kernel_size // 2,
-                          self.kernel_size // 2, self.kernel_size // 2),
-                  mode='constant', value=0)  # (N, C, H+Kup//2+Kup//2, W+Kup//2+Kup//2)
+        x = F.pad(
+            x,
+            pad=(self.kernel_size // 2, self.kernel_size // 2, self.kernel_size // 2, self.kernel_size // 2),
+            mode="constant",
+            value=0,
+        )  # (N, C, H+Kup//2+Kup//2, W+Kup//2+Kup//2)
         x = x.unfold(2, self.kernel_size, step=1)  # (N, C, H, W+Kup//2+Kup//2, Kup)
         x = x.unfold(3, self.kernel_size, step=1)  # (N, C, H, W, Kup, Kup)
         x = x.reshape(N, C, H, W, -1)  # (N, C, H, W, Kup^2)
@@ -48,5 +50,6 @@ class CARAFE(nn.Module):
         out_tensor = self.out(out_tensor)
         # print("up shape:",out_tensor.shape)
         return out_tensor
+
 
 ######################  CARAFE   ####     end     ###############################
